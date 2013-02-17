1b46392c488a2135db3b1d4e8072d0b4c6a528bf
Preston Maness <aggroskater@gmail.com>
2013-02-17 02:18:16 -0600

    updating main.cpp to stack assignment

---
+++ lab2/main.cpp
@@ -0,0 +1,51 @@
+/* Need to get input from user that has the data for all of the students. Ought
+   to use input file. Easier to parse and grab from command line.
+*/
+
+#include "stack.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void addFileToList(ifstream&,stack*);
+
+int main(int argc, char *argv[])
+{
+
+  stack* st = new stack;
+  ifstream ifs;
+  ifs.open("input.txt");
+  addFileToList(ifs, st);
+
+  return 0;
+
+}
+
+void addFileToList(ifstream& input,stack* st)
+{
+
+  while( input.good() && input.peek() != EOF )
+  {
+
+    string id,name,address,gpastring;
+    getline(input,id,';');
+    getline(input,name,';');
+    getline(input,address,';');
+    getline(input,gpastring,'\n');
+
+    double gpa;
+    gpa = atof(gpastring.c_str());
+
+    Student *temp = new Student(id,name,address,gpa);
+//    list->insertTail(temp);
+    st->push(temp);
+
+  }
+
+  /* Put ifs seeker back to beginning of file stream */
+  input.seekg(0, ios::beg);
+
+}
