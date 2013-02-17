a02c3430b6f290dda906a0467c939f3d45dbb4bf
Preston Maness <aggroskater@gmail.com>
2013-02-17 02:14:04 -0600

    initial commiting.

---
+++ lab2/stack.h
@@ -0,0 +1,24 @@
+#ifndef stack_h_
+#define stack_h_
+
+#include "Student.h"
+
+class stack
+
+{
+
+  private:
+
+    Student stack[100]; /* 100 stack frames oughta be enough for anybody... */
+
+  public:
+
+    stack();
+    virtual ~stack();
+
+    void push();
+    void pop();
+
+}
+
+#endif
